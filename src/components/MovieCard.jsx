591573bb667f177e310ea315d899c03764a0b0bf
Idansulimanov20 <idansolimanov22@gmail.com>
2025-11-15 23:48:25 +0200

    fix: enhance MovieCard component to handle missing images and update no-image styles

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,21 +1,31 @@
 import "../css/MovieCard.css";
 import { useMovieContext } from "../context/useMovieContext";
+
 function MovieCard({ movie }) {
   const { isFavorite, addToFavorites, removeFromFavorites } = useMovieContext();
   const favorite = isFavorite(movie.id);
+
   function onFavoriteClick(e) {
     e.preventDefault();
     if (favorite) removeFromFavorites(movie.id);
     else addToFavorites(movie);
   }
 
+  const imageUrl = movie.poster_path
+    ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+    : null;
+
   return (
     <div className="movie-card">
       <div className="movie-poster">
-        <img
-          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-          alt={movie.title}
-        />
+        {imageUrl ? (
+          <img src={imageUrl} alt={movie.title} />
+        ) : (
+          <div className="no-image">
+            <span>Image Not Available</span>
+          </div>
+        )}
+
         <div className="movie-overlay">
           <button
             className={`favorite-btn ${favorite ? "active" : ""}`}
@@ -25,6 +35,7 @@ function MovieCard({ movie }) {
           </button>
         </div>
       </div>
+
       <div className="movie-info">
         <h3>{movie.title}</h3>
         <p>{movie.release_date?.split("-")[0]}</p>
